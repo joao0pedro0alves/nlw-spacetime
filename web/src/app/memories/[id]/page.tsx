76cabfa1fb985c6c8814cd8ad40106a0da69462c
João Alves <joao.alves@ecosis.com.br>
2023-07-07 15:10:19 -0300

    chore: create memory page

---
+++ web/src/app/memories/[id]/page.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { cookies } from 'next/headers'
+
+import dayjs from 'dayjs'
+
+import { ChevronLeft } from 'lucide-react'
+import { api } from '@/lib/api'
+
+interface MemoryProps {
+  params: { id: string }
+}
+
+export default async function Memory({ params }: MemoryProps) {
+  const token = cookies().get('token')?.value
+
+  const response = await api.get(`/memories/${params.id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const memory = response.data
+
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-16">
+      <Link
+        href="/"
+        className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
+      >
+        <ChevronLeft className="h-4 w-4" />
+        voltar à timeline
+      </Link>
+
+      <div className="space-y-4">
+        <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+          {dayjs(memory.createdAt).format('DD [de] MMMM, YYYY')}
+        </time>
+        <Image
+          src={memory.coverUrl}
+          alt=""
+          width={592}
+          height={280}
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+        <p className="text-lg leading-relaxed text-gray-100">
+          {memory.content}
+        </p>
+      </div>
+    </div>
+  )
+}
